6c89f2d2acd50948f0782020f863ee9ca7681e1b
ravi singh <ravics0027@gmail.com>
2019-04-03 10:28:33 +0530

    initial commit

---
+++ assignments/codding challange/compare-string/script.js
@@ -0,0 +1,36 @@
+var str1="javascript";
+var str2="navascript";
+var count=0;
+var comarestring=function(str1,str2)
+
+{
+  if(str1.length == str2.length){
+      for(var i=0;i<=str1.length;i++){
+        if(str1.charAt(i) != str2.charAt(i)){
+          count=count+1;
+        }
+      
+      }
+    }
+return count
+}
+var str3="javascript";
+var str4="novascript";
+var count=0;
+var comarestring=function(str1,str2)
+
+{
+  if(str1.length == str2.length){
+      for(var i=0;i<=str1.length;i++){
+        if(str1.charAt(i) != str2.charAt(i)){
+          count=count+1;
+        }
+      
+      }
+    }
+return count
+}
+console.log(comarestring(str1,str2));  
+
+console.log(comarestring(str1,str2));  
+
