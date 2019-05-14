9c0cff174c63725f47d2c7e6095c81470b8d6ab2
ravi singh <ravics0027@gmail.com>
2019-05-14 20:15:56 +0530

    react-app

---
+++ assignments/codding challange/ispoweroftwo.js
@@ -0,0 +1,18 @@
+var isPowerOfTwo = function(num) {
+
+  n = num;
+  
+  while(n%2 == 0){
+    
+    n = Math.floor(n/2);
+    
+  if(n == 1){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+console.log(isPowerOfTwo(32))
+
+console.log(isPowerOfTwo(9))
