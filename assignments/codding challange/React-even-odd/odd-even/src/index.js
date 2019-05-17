7c29b7554ed350cfbe2e07c400841e573fb4207f
ravi singh <42317356+ravics0027@users.noreply.github.com>
2019-05-17 12:38:23 +0530

    Update index.js

--- assignments/codding challange/React-even-odd/odd-even/src/index.js
+++ assignments/codding challange/React-even-odd/odd-even/src/index.js
@@ -2,15 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Input extends React.Component {
-    checkOddEven() {
-        if (this.state.number % 2 == 0) {
-            alert(" Even")
-        } else {
-            alert("not Even");
-        }
-    }
-
-
+    
     render() {
         return (
             <div>
@@ -33,26 +25,7 @@ class Button extends React.Component {
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-            number: 0
-        }
-        this.state = {
-            title: "even odd numner"
-        }
-
-        this.getNumber = this.getNumber.bind(this);
-        this.checkOddEven = this.checkOddEven.bind(this);
-
-    }
-
-getNumber(event) {
-       
-    this.setState({ number: event.target.value });
-}
-
+   
 
     
     render() {
@@ -60,7 +33,7 @@ getNumber(event) {
         return (
 
             <div className="container">
-                <h1>{this.state.title}</h1>
+               
 
 
                 <Input onChange={this.getNumber} />
