6cf827c2f57b1d4969f486aa54a1c2e407b51022
ravi singh <42317356+ravics0027@users.noreply.github.com>
2019-05-27 13:55:07 +0530

    Update index.js

--- assignments/week-14/rout/src/index.js
+++ assignments/week-14/rout/src/index.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as router,Route,Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
+
       <Router>
-      <div className="container-fluid">
+        <div className="container-fluid">
 
-        <div className="row">
-          <div className="col-md-4 offset-md-4">
-            <ul class="nav justify-content-center">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About Us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact Us</a>
-              </li>
+          <div className="row">
+            <div className="col-md-4 offset-md-4">
+              <ul class="nav justify-content-center">
+                <li class="nav-item">
+                  <a class="nav-link active" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/about">About Us</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/contact">Contact Us</a>
+                </li>
 
 
-            </ul>
+              </ul>
+            </div>
+
           </div>
 
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+
+
         </div>
-       
-<Route path="/" component={Home}/>
-<Route path="/about" component={About}/>
-<Route path="/contact" component={Contact}/>
 
-
-      </div>
-
-</Router>
-    );
+      </Router>
+    )
   }
 }
 class Home extends React.Component {
@@ -44,8 +45,8 @@ class Home extends React.Component {
       <div className="row">
         <div className="col-md-4 offset-md-4">
           <h2>AttainU Phoenix</h2>
-<p>we are AttainU Phoenix
-  we are developer
+          <p>we are AttainU Phoenix
+            we are developer
 </p>
         </div>
 
@@ -64,8 +65,8 @@ class About extends React.Component {
       <div className="row">
         <div className="col-md-4 offset-md-4">
           <h2>AttainU Phoenix</h2>
-<p>we are AttainU Phoenix
-  we are developer
+          <p>we are AttainU Phoenix
+            we are developer
 </p>
         </div>
 
@@ -84,9 +85,9 @@ class Contact extends React.Component {
     return (
       <div className="row">
         <div className="col-md-4 offset-md-4">
-         
-<p>AttainU Phoenix
-  for hire please Contact Us
+
+          <p>AttainU Phoenix
+            for hire please Contact Us
 </p>
         </div>
 
