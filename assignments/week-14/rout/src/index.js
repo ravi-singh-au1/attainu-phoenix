c11f03b5d10cb6f92f5873c70f5f850139e84a2e
ravi singh <42317356+ravics0027@users.noreply.github.com>
2019-05-27 13:59:50 +0530

    Update index.js

--- assignments/week-14/rout/src/index.js
+++ assignments/week-14/rout/src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
             <div className="col-md-4 offset-md-4">
               <ul class="nav justify-content-center">
                 <li class="nav-item">
-                  <a class="nav-link active" href="/">Home</a>
+                  <a class="nav-link active" href="/home">Home</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="/about">About Us</a>
@@ -28,7 +28,7 @@ class App extends React.Component {
 
           </div>
 
-          <Route path="/" component={Home} />
+          <Route path="/home" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
 
@@ -64,7 +64,7 @@ class About extends React.Component {
     return (
       <div className="row">
         <div className="col-md-4 offset-md-4">
-          <h2>AttainU Phoenix</h2>
+          <h2>About :AttainU Phoenix</h2>
           <p>we are AttainU Phoenix
             we are developer
 </p>
@@ -86,7 +86,7 @@ class Contact extends React.Component {
       <div className="row">
         <div className="col-md-4 offset-md-4">
 
-          <p>AttainU Phoenix
+          <p>Contact us:-
             for hire please Contact Us
 </p>
         </div>
