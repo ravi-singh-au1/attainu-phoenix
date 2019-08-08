fb1e368a0edb3b95ce8e1ea240918cac0e16f6ee
ravi singh <ravics0027@gmail.com>
2019-08-08 20:21:09 +0530

    print largest ans small array with function

---
+++ codding-challange/largest and small/index.js
@@ -0,0 +1,16 @@
+ 
+alert('hello open console here')
+// create a function print largest and smallest number
+var numbers =[8,9,54,65,24,78,3];
+var sorted = numbers.slice().sort(function(a,b)
+{
+    return a -b ;
+
+});
+var smallest = sorted[0];
+secondSmallest = sorted[1];
+largest =sorted[sorted.length-1];
+
+console.log('smallest =>' + smallest);
+console.log('secondsmallest =>' + secondSmallest);
+console.log('largest =>' + largest);
