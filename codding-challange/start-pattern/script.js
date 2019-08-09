20ec9c4627dd4e78437ffc7cdf6d9c3054034197
ravi singh <ravics0027@gmail.com>
2019-08-10 02:42:49 +0530

    start pattern

---
+++ codding-challange/start-pattern/script.js
@@ -0,0 +1,24 @@
+
+
+
+var i, j;
+
+for(i=1; i <= 10; i++)
+ {
+
+  for(j=1; j<=i; j++)
+ {
+   document.write('*');
+  }
+   document.write('<br/>');
+ }
+ 
+ for(i=10;i>=1;i--)
+ {
+
+  for(j=1;j<=i;j++)
+ {
+   document.write('*');
+  }
+   document.write('<br/>');
+}
