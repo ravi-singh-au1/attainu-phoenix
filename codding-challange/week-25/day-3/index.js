14f61fa9fe4819b2aa2dcf717e541a55b384f112
ravi singh <ravics0027@gmail.com>
2019-08-14 09:34:13 +0530

    isUnique String

---
+++ codding-challange/week-25/day-3/index.js
@@ -0,0 +1,11 @@
+function is_unique(str) {
+    var obj = {};
+    for(var z=0;z<str.length;++z) {
+      var ch = str[z];
+      if(obj[ch]) return false; else obj[ch] = true;
+    }
+    return true;
+  }
+  // test:
+  console.log(is_unique("ansal")); // true
+  console.log(is_unique("arkesh")); // false
