4870473e025d2b2de35bf48be73ea2cf186ba0df
ravi singh <ravics0027@gmail.com>
2019-08-19 09:23:09 +0530

    merged and sort

---
+++ codding-challange/week-26/day-1/index.js
@@ -0,0 +1,11 @@
+function merged()
+{
+var array1 = ["b"];
+var array2 = ["a"];
+var result = array1.concat(array2);
+console.log(result);
+var sort = result.sort();
+console.log(sort);
+}
+
+merged();
