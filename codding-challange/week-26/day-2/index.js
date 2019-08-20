1de52bcc5c137110a0ebf11de6a6e1f47c4828ea
ravi singh <ravics0027@gmail.com>
2019-08-20 11:37:20 +0530

    Permutation of string

---
+++ codding-challange/week-26/day-2/index.js
@@ -0,0 +1,18 @@
+var permArr = [],
+  usedChars = [];
+
+function Permutation(input) {
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length == 0) {
+      permArr.push(usedChars.slice());
+    }
+    Permutation(input);
+    input.splice(i, 0, ch);
+    usedChars.pop();
+  }
+  return permArr
+};
+console.log(Permutation(["A", "B" , "C"]));
