cc2caf5f6a5f294ff878b6e30e83316a0baa87ca
ravi singh <ravics0027@gmail.com>
2019-09-10 10:45:55 +0530

    reverse ofdigits

--- codding-challange/week-29/day-2/index.js
+++ codding-challange/week-29/day-2/index.js
@@ -1,22 +1,22 @@
 function reverseDigit(num)
 {
-
-
     var rev_num = 0;
     while(num > 0)
     {
         rev_num = rev_num * 10 + num % 10;
         num = Math.floor(num / 10);
+       
     }
     return rev_num;
-   
-
 }
 var num = 123 ;
 var num2 = 521;
 var num3 = 120 ;
-
+var num4 = -120
+var num5 = -10
 
 console.log(reverseDigit(num));
 console.log(reverseDigit(num2));
 console.log(reverseDigit(num3));
+console.log(reverseDigit(Math.abs(num4)));
+console.log(reverseDigit(Math.abs(num5)));
