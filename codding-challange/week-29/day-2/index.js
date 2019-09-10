99bd3e67eab011603d7a374d959194a94ef0bd76
ravi singh <ravics0027@gmail.com>
2019-09-10 10:20:26 +0530

    reverse of digit

---
+++ codding-challange/week-29/day-2/index.js
@@ -0,0 +1,17 @@
+function reverseDigit(num)
+{
+
+
+    var rev_num = 0;
+    while(num > 0)
+    {
+        rev_num = rev_num * 10 + num % 10;
+        num = Math.floor(num / 10);
+    }
+    return rev_num;
+   
+
+}
+var num = 123;
+
+console.log(reverseDigit(num));
