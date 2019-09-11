63425bcc3ea6a1c2c7c241f5a641f1571f940245
ravi singh <ravics0027@gmail.com>
2019-09-11 11:21:13 +0530

    Covert to Roman

---
+++ codding-challange/week-29/day-3/convertRoman.js
@@ -0,0 +1,31 @@
+function convertToRoman(num) {
+    var roman = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    };
+    var str = '';
+  
+    for (var i of Object.keys(roman)) {
+      var q = Math.floor(num / roman[i]);
+      num -= q * roman[i];
+      str += i.repeat(q);
+    }
+  
+    return str;
+  }
+
+ console.log(convertToRoman(9));
+ console.log(convertToRoman(10));
+ console.log(convertToRoman(1994));
+ console.log(convertToRoman(58));
